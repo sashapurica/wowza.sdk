e6d0b3de93e07b842779811075084b2ea2943c5f
Carlos Camacho <carlos.camacho@frontiersin.org>
2015-12-02 12:29:25 +0100

    config fix

--- wowza.io.php/examples/exampleCRUD/config.php
+++ wowza.io.php/examples/exampleCRUD/config.php
@@ -5,6 +5,10 @@
 //    created: November 2015
 
 
+//In windows: add the vars to advanced properties
+//In linux:   add the vars to /etc/apache2/envvars
+
+
 define("STREAMING_SERVER_IP", getenv("WOWZA_SERVER_ADDRESS"));
 define("STREAMING_SERVER_PORT", getenv("WOWZA_SERVER_PORT"));
 define("STREAMING_SERVER_DELIVERY_PORT", getenv("WOWZA_SERVER_DELIVERY_PORT"));
