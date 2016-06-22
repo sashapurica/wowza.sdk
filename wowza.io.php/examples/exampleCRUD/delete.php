dee041bc19576969d7eeb919d49da8a92adca864
sashapurica <sashapurica@gmail.com>
2016-06-21 23:16:01 -0430

    update delete.php
    update file header to GNU GPLv3

--- wowza.io.php/examples/exampleCRUD/delete.php
+++ wowza.io.php/examples/exampleCRUD/delete.php
@@ -21,7 +21,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  *
- // @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+   @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
  */
 
 
