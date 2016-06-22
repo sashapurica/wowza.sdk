4c7507d60fbf09d57bffced7985875f5f216b21c
sashapurica <sashapurica@gmail.com>
2016-06-21 23:07:35 -0430

    update config.php
    update file header to GNU GPLv3

--- wowza.io.php/examples/exampleCRUD/config.php
+++ wowza.io.php/examples/exampleCRUD/config.php
@@ -3,10 +3,28 @@
 //    author: Carlos Camacho
 //    email: carloscamachoucv@gmail.com
 //    created: November 2015
-
-
 //In windows: add the vars to advanced properties
 //In linux:   add the vars to /etc/apache2/envvars
+/**
+ * Copyright by wowza.io at http://www.wowza.io
+ *
+ * This file is part of wowza.io SDK
+ * 
+ * Some open source application is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * Some open source application is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ */
 
 
 define("STREAMING_SERVER_IP", getenv("WOWZA_SERVER_ADDRESS"));
